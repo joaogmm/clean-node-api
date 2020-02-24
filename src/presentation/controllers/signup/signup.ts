ec041236907f783d2cc9dd59d93cbf23cdab374b
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-02-24 20:26:06 -0300

    refactor: move all singup protocols to a single file

---
+++ src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,39 @@
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helpers'
+
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  private readonly AddAccount: AddAccount
+
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+    this.emailValidator = emailValidator
+    this.AddAccount = addAccount
+  }
+
+  handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      this.AddAccount.add({
+        name,
+        email,
+        password
+      })
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
