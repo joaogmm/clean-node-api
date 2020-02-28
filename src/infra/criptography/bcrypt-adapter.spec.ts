9c99dbe64d24b296f2820eb7c99051e88c89c18d
Joao Gabriel de Morais Magalhaes <joaomagalhaes@ibm.com>
2020-02-27 22:41:08 -0300

    feat: ensure BCryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -43,4 +43,10 @@ describe('BCrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    await expect(isValid).toBe(true)
+  })
 })
